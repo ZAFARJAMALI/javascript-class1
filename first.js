5d056392ad66ace0381e6c7cd44110cd2f795f9f
ZAFARJAMALI <157882201+ZAFARJAMALI@users.noreply.github.com>
2024-08-31 02:22:52 -0700

    Javascript class1

---
+++ first.js
@@ -0,0 +1,75 @@
+
+// console.log("well come zafar jamali");
+// console.log("hello java ");
+// console.log("i am using java");
+// fullName = "zafar jamali";
+// age=24;
+// console.log(fullName,age);
+
+// isFollow =true;
+
+let brandNmae= "Adidas";
+let warranty ="two years"
+let totalPrice =100;
+
+console.log(brandNmae,warranty,totalPrice);
+
+const student={
+
+name: "zafar",
+age:24,
+semester: "fourth",
+gpa: 3.96,
+isPass:true
+};
+student["age"] = student["age"] -5
+console.log(student.age);
+
+// PRACTICE VARIABLE / Object
+// const product ={Name : "parker jotter standard CT ball pen(black)",
+
+// Rating : 4,
+// Offer : 5,
+// price : 270 ,
+// };
+// console.log(product)
+
+const profile = {
+
+    userName : "shrada khapra",
+    posts : 195,
+following : 569,
+isFollow : false,
+bio : "apna college | Ex_microsoft | to educate someone is the highest privilege",
+
+};
+console.log (profile)
+
+
+const userenter = prompt("please enter page password");
+ const facebookPage ={
+    
+name:"zafar jamali",
+password:12345678,
+
+followers:1300,
+following :2,
+post:213,
+isFollow:true,
+bio:"hey my name is zafar jamali",
+
+ };
+ if (facebookPage.password == userenter){
+    alert("page is valid");
+} else {
+    alert("page is invalid") ;
+   
+ }
+
+// var password = '12345678'
+// var userinter = prompt("plz enter password")
+// if (password === userinter) {
+//     alert("password is valid")
+// } else {
+//     alert('password is invalid')
+
